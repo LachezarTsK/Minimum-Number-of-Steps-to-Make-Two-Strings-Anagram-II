f60e6d0251fb70fd8de3f8d8ad01486a96666d4f
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-05-05 18:47:58 +0200

    Create Solution.cs

---
+++ Solution.cs
@@ -0,0 +1,32 @@
+
+using System;
+
+public class Solution
+{
+    static readonly int ALPHABET_SIZE = 26;
+
+    public int MinSteps(string firstInput, string secondInput)
+    {
+        int[] frequency = new int[ALPHABET_SIZE];
+        foreach (char letter in firstInput)
+        {
+            ++frequency[letter - 'a'];
+        }
+        foreach (char letter in secondInput)
+        {
+            --frequency[letter - 'a'];
+        }
+
+        return calculateMinSteps(frequency);
+    }
+
+    private int calculateMinSteps(int[] frequency)
+    {
+        int minSteps = 0;
+        foreach (int current in frequency)
+        {
+            minSteps += Math.Abs(current);
+        }
+        return minSteps;
+    }
+}
